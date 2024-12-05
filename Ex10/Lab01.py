0db5ad762dcce3051cc5d637ecc1aba67ba49b53
SY Lee <seoyong.lee@libertas-scholars.org>
2024-12-05 09:32:01 +0900

    lab finished

---
+++ Ex10/Lab01.py
@@ -0,0 +1,20 @@
+import random
+'''
+You will be given a list, TARGET, which consists of alphabets from A to Z.
+Some of them are lower cases while others are upper cases. 
+Make a lambda function which replaces lowercases with uppercases and vice versa.
+It should return its result as a list at the end. 
+'''
+lower = lambda:list(chr(each) for each in range(65, 91))
+upper = lambda:list(chr(each) for each in range(97, 123))
+lowerCase = lower()
+upperCase = upper()
+mix = lambda:list(lowerCase[index] if random.randint(1,2) == 1 else upperCase[index] for index in range(26))
+TARGET = mix()
+# print(TARGET)
+
+repLower = lambda:list(each if 65 <= ord(each) <= 91 else chr(ord(each) - 32) for each in TARGET)
+print(repLower())
+
+repUpper = lambda:list(each if 97 <= ord(each) <= 123 else chr(ord(each)+32) for each in TARGET)
+print(repUpper())
--- SampleTime.py
+++ SampleTime.py
@@ -1,13 +1,12 @@
 import datetime as dt
 
-current = dt.datetime.now()
-# print(current.hour)
-# print(type(current.hour))
-
 def whatT():
+    
+    current = dt.datetime.now()
     ch = current.hour
+    
     if ch > 12:
-        ch = ch - 12
+        # ch = ch - 12
         print("오후 ", ch, "시 입니다")
     else:
         print("오전 ", ch, "시 입니다")
